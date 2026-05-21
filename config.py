b14356b54c4c7e9648d35d7f8181a836cefddaa7
vn99instrumental-web <vn99instrumental@gmail.com>
2026-05-21 11:07:33 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+from datetime import timezone, timedelta
+
+ICT           = timezone(timedelta(hours=7))
+MARKET_OPEN   = (9, 0)
+MARKET_CLOSE  = (15, 0)
+LOOKBACK_DAYS = 90
+TOP_N         = 10
+DEEP_N        = 3
+HOSE_CODE     = "HSX"
+OUTPUT_DIR    = "output"
