497754e668c92d9cb93e86f36c482740f6429a94
vn99instrumental-web <vn99instrumental@gmail.com>
2026-06-12 23:52:07 +0700

    Delete scripts directory

--- scripts/debug_rss.py
+++
@@ -1,4 +0,0 @@
-from vnstock_news import SITES_CONFIG
-for site, cfg in SITES_CONFIG.items():
-    has_rss = bool(cfg.get('rss_urls') or cfg.get('rss') or cfg.get('feed_url'))
-    print(f"{site:<30} RSS={has_rss}")
